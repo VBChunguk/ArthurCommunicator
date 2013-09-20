d732098199e74915f10970dcba40c78885486499
VBChunguk <chwo9843@gmail.com>
2013-09-20 12:06:15 +0900

    Move code to git

---
+++ ArthurCommunicator/HttpRequestClient.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace Zotca.Vbc.Comm
+{
+    /// <summary>
+    /// 내부에서 암호화를 수행하여 서버와 통신합니다.
+    /// </summary>
+    public class HttpRequestClient
+    {
+        private string mHost;
+        private ushort mPort;
+
+        private int mAppVersion;
+        private string mDeviceName;
+        private string mProductName;
+        private string mOSVersion;
+        private string mFingerprint;
+
+        private string mCookie;
+
+        /// <summary>
+        /// 기본값으로 지정된 서버로 접속하는 클라이언트를 만듭니다.
+        /// </summary>
+        public HttpRequestClient()
+        {
+            Initialize("ma.actoz.com", 10001);
+        }
+
+        /// <summary>
+        /// 특정 호스트와 기본값으로 지정된 포트를 사용해 서버로 접속하는 클라이언트를 만듭니다.
+        /// </summary>
+        /// <param name="host">접속할 호스트입니다.</param>
+        public HttpRequestClient(string host)
+        {
+            Initialize(host, 10001);
+        }
+
+        /// <summary>
+        /// 특정 호스트와 포트를 사용해 서버로 접속하는 클라이언트를 만듭니다.
+        /// </summary>
+        /// <param name="host">접속할 호스트입니다.</param>
+        /// <param name="port">접속할 포트입니다.</param>
+        public HttpRequestClient(string host, ushort port)
+        {
+            Initialize(host, port);
+        }
+
+        /// <summary>
+        /// 주어진 호스트와 포트를 사용해 개체를 초기화합니다.
+        /// </summary>
+        /// <param name="host">접속할 호스트입니다.</param>
+        /// <param name="port">접속할 포트입니다.</param>
+        private void Initialize(string host, ushort port)
+        {
+            mHost = host;
+            mPort = port;
+            mAppVersion = 103;
+            mDeviceName = mProductName = mOSVersion = mFingerprint = string.Empty;
+            mCookie = string.Empty;
+        }
+
+        /// <summary>
+        /// 접속에 사용할 앱 버전을 가져오거나 설정합니다.
+        /// </summary>
+        public int AppVersion
+        {
+            get { return mAppVersion; }
+            set { mAppVersion = value; }
+        }
+
+        /// <summary>
+        /// 접속에 사용할 Build.DEVICE 값을 가져오거나 설정합니다.
+        /// </summary>
+        public string DeviceName
+        {
+            get { return mDeviceName; }
+            set { mDeviceName = value; }
+        }
+
+        /// <summary>
+        /// 접속에 사용할 Build.PRODUCT 값을 가져오거나 설정합니다.
+        /// </summary>
+        public string ProductName
+        {
+            get { return mProductName; }
+            set { mProductName = value; }
+        }
+
+        /// <summary>
+        /// 접속에 사용할 Build.VERSION.RELEASE 값을 가져오거나 설정합니다.
+        /// </summary>
+        public string OSVersion
+        {
+            get { return mOSVersion; }
+            set { mOSVersion = value; }
+        }
+
+        /// <summary>
+        /// 접속에 사용할 Build.FINGERPRINT 값을 가져오거나 설정합니다.
+        /// </summary>
+        public string Fingerprint
+        {
+            get { return mFingerprint; }
+            set { mFingerprint = value; }
+        }
+
+        /// <summary>
+        /// 접속에 사용할 User-Agent를 가져옵니다.
+        /// </summary>
+        public string UserAgent
+        {
+            get { return string.Format("Million/{0} ({1}; {2}; {3}) {4}", mAppVersion, mDeviceName, mProductName, mOSVersion, mFingerprint); }
+        }
+
+        /// <summary>
+        /// 서버에 POST 요청을 보냅니다.
+        /// </summary>
+        /// <param name="endpoint">요청을 보낼 끝점입니다.</param>
+        /// <param name="args">키와 값 쌍으로 이루어진 요청의 인수입니다.</param>
+        /// <returns>요청에 대한 응답입니다.</returns>
+        public byte[] RequestPost(string endpoint, Dictionary<string, string> args)
+        {
+            HttpWebRequest req = HttpWebRequest.Create("http://" + mHost + endpoint + "?cyt=1") as HttpWebRequest;
+            {
+                Dictionary<string, string> convertedArgs = new Dictionary<string, string>();
+                if (args != null)
+                {
+                    foreach (KeyValuePair<string, string> item in args)
+                    {
+                        string val = Utils.Crypt.EncryptAndBase64(Encoding.UTF8.GetBytes(item.Value));
+                        convertedArgs[item.Key] = val;
+                    }
+                }
+                req.Credentials = new NetworkCredential("iW7B5MWJ", "8KdtjVfX", mHost);
+                req.UserAgent = UserAgent;
+                req.Headers.Add("Accept-Encoding", "gzip, deflate");
+                req.Headers.Add("Cookie", mCookie);
+                req.Method = "POST";
+                Stream entity = req.GetRequestStream();
+                byte[] bodyBytes = Encoding.ASCII.GetBytes(MakeRequestBody(convertedArgs));
+                entity.Write(bodyBytes, 0, bodyBytes.Length);
+            }
+
+            HttpWebResponse responseObj = req.GetResponse() as HttpWebResponse;
+            mCookie = responseObj.Headers.Get("Set-Cookie");
+
+            Stream stream = responseObj.GetResponseStream();
+            byte[] response = Utils.Crypt.Decrypt(stream);
+            if (responseObj.ContentEncoding != null && responseObj.ContentEncoding.Contains("gzip"))
+            {
+                try
+                {
+                    MemoryStream responseStream = new MemoryStream(response);
+                    responseStream.Position = 0;
+                    GZipStream gzipStream = new GZipStream(responseStream, CompressionMode.Decompress);
+                    MemoryStream decodeStream = new MemoryStream();
+                    while (true)
+                    {
+                        byte[] buffer = new byte[256];
+                        int read = gzipStream.Read(buffer, 0, 256);
+                        if (read <= 0) break;
+                        decodeStream.Write(buffer, 0, read);
+                    }
+                    decodeStream.Position = 0;
+                    response = new byte[decodeStream.Length];
+                    decodeStream.Read(response, 0, response.Length);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            responseObj.Close();
+            return response;
+        }
+
+        /// <summary>
+        /// 주어진 인수로부터 요청 본문을 만듭니다.
+        /// </summary>
+        /// <param name="args">요청 본문을 만들 키와 값 쌍으로 이루어진 인수입니다.</param>
+        /// <returns>요청 본문입니다.</returns>
+        private string MakeRequestBody(Dictionary<string, string> args)
+        {
+            StringBuilder bodyBuilder = new StringBuilder();
+            bool first = true;
+            foreach (KeyValuePair<string, string> item in args)
+            {
+                if (!first) bodyBuilder.Append('&');
+                bodyBuilder.Append(WebUtility.UrlEncode(item.Key));
+                bodyBuilder.Append('=');
+                bodyBuilder.Append(WebUtility.UrlEncode(item.Value));
+                first = false;
+            }
+            return bodyBuilder.ToString();
+        }
+    }
+}
---
+++ ArthurCommunicator/Utils/Crypt.cs
@@ -0,0 +1,117 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Zotca.Vbc.Comm.Utils
+{
+    /// <summary>
+    /// 암호화를 수행하는 정적 클래스입니다.
+    /// </summary>
+    public static class Crypt
+    {
+        /// <summary>
+        /// 바이트 배열로부터 데이터를 복호화합니다.
+        /// </summary>
+        /// <param name="data">복호화할 바이트 배열입니다.</param>
+        /// <returns>복호화된 데이터입니다.</returns>
+        public static byte[] Decrypt(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream(data);
+            return Decrypt(stream);
+        }
+
+        /// <summary>
+        /// System.IO.Stream으로부터 데이터를 복호화합니다.
+        /// </summary>
+        /// <param name="data">복호화할 System.IO.Stream입니다.</param>
+        /// <returns>복호화된 데이터입니다.</returns>
+        public static byte[] Decrypt(Stream data)
+        {
+            Aes aes = Aes.Create();
+            aes.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            aes.Key = Encoding.ASCII.GetBytes("rBwj1MIAivVN011i");
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.ECB;
+            ICryptoTransform aesTransform = aes.CreateDecryptor();
+            CryptoStream cryptStream = new CryptoStream(data, aesTransform, CryptoStreamMode.Read);
+            MemoryStream aesOutputStream = new MemoryStream();
+            while (true)
+            {
+                byte[] buffer = new byte[256];
+                int read = cryptStream.Read(buffer, 0, 256);
+                if (read <= 0) break;
+                aesOutputStream.Write(buffer, 0, read);
+            }
+            aesOutputStream.Position = 0;
+            byte[] ret = new byte[aesOutputStream.Length];
+            aesOutputStream.Read(ret, 0, ret.Length);
+            return ret;
+        }
+
+        /// <summary>
+        /// 바이트 배열로부터 데이터를 암호화합니다.
+        /// </summary>
+        /// <param name="valBytes">암호화할 바이트 배열입니다.</param>
+        /// <returns>암호화된 데이터입니다.</returns>
+        public static byte[] Encrypt(byte[] valBytes)
+        {
+            Aes aes = Aes.Create();
+            aes.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            aes.Key = Encoding.ASCII.GetBytes("rBwj1MIAivVN011i");
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.ECB;
+            ICryptoTransform aesTransform = aes.CreateEncryptor();
+            MemoryStream aesFinishedStream = new MemoryStream();
+            CryptoStream cryptStream = new CryptoStream(aesFinishedStream, aesTransform, CryptoStreamMode.Write);
+            cryptStream.Write(valBytes, 0, valBytes.Length);
+            cryptStream.FlushFinalBlock();
+
+            aesFinishedStream.Position = 0;
+            byte[] ret = new byte[aesFinishedStream.Length];
+            aesFinishedStream.Read(ret, 0, ret.Length);
+            return ret;
+        }
+
+        /// <summary>
+        /// 바이트 배열로부터 Base64 인코딩을 수행합니다.
+        /// </summary>
+        /// <param name="val">인코딩할 바이트 배열입니다.</param>
+        /// <returns>Base64 인코딩된 데이터입니다.</returns>
+        public static string Base64(byte[] val)
+        {
+            ToBase64Transform b64Transform = new ToBase64Transform();
+            MemoryStream b64FinishedStream = new MemoryStream();
+            int len = val.Length;
+            int p = 0;
+            while (true)
+            {
+                byte[] outputBuffer = new byte[b64Transform.OutputBlockSize];
+                int read = len - p;
+                if (read < b64Transform.InputBlockSize)
+                {
+                    outputBuffer = b64Transform.TransformFinalBlock(val, p, read);
+                    b64FinishedStream.Write(outputBuffer, 0, outputBuffer.Length);
+                    break;
+                }
+                else
+                {
+                    b64Transform.TransformBlock(val, p, b64Transform.InputBlockSize, outputBuffer, 0);
+                    b64FinishedStream.Write(outputBuffer, 0, b64Transform.OutputBlockSize);
+                }
+                p += read;
+            }
+            return Encoding.ASCII.GetString(b64FinishedStream.GetBuffer(), 0, (int)b64FinishedStream.Length);
+        }
+
+        /// <summary>
+        /// 바이트 배열로부터 데이터를 암호화하고 Base64 인코딩을 수행합니다.
+        /// </summary>
+        /// <param name="val">암호화할 바이트 배열입니다.</param>
+        /// <returns>암호화 후 Base64 인코딩된 데이터입니다.</returns>
+        public static string EncryptAndBase64(byte[] val)
+        {
+            byte[] valBytes = Encrypt(val);
+            return Base64(valBytes);
+        }
+    }
+}
